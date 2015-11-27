a073fb0fb5e86547d4a55a81ec95481569a7e905
Jonathan Sick <jsick@lsst.org>
2015-11-27 15:28:48 -0700

    Build sass with gulp sass
    Uses the node-sass plugin. Keeping the sass source in sass/ and putting
    the products into the theme's static/css file.
    
    We're ignoring the build directory.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 'use strict';
 
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
 gulp.task('hello', function() {
     console.log('Hello Jonathan!');
+});
+
+gulp.task('sass', function() {
+    return gulp.src('sass/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('lsst_sphinx_technote_theme/static/css'))
 });
