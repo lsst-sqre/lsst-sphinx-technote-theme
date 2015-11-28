ff17847c428e4b5da6e3f3ec16101ff938cb9eb0
Jonathan Sick <jsick@lsst.org>
2015-11-27 19:54:01 -0700

    Use customPlumber error handling from AYW book
    Uses gulp-plumber to catch errors and prevent them from crashing a gulp
    watch. Instead, gulp gracefully prints the errors and waits for you to
    correct it.
    
    From Chapter 9 of Automate your Workflow by Zell Liew.

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,34 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
 
 gulp.task('hello', function() {
     console.log('Hello Jonathan!');
 });
 
+
+// gulp-plummer wrapper to handle errors during gulp watch
+// From Zell Liew. “Automate Your Workflow.”
+function customPlumber () {
+    return plumber({
+        errorHandler: function(err) {
+            // Logs error in console
+            console.log(err.stack);
+            // Ends the current pipe, so Gulp watch doesn't break
+            this.emit('end');
+        }
+    });
+}
+
 // gulp sass - build Sass in sass/ directory and install into theme
 gulp.task('sass', function() {
     return gulp.src('sass/**/*.scss')
+        .pipe(customPlumber())
         .pipe(sass())
         .pipe(gulp.dest('lsst_sphinx_technote_theme/static/css'))
 });
+
 
 // gulp watch - build when files change
 // sass is run automatically when watch is run; even before files are changed
