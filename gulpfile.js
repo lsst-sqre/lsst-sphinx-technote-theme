70ed6c295601d398edad30f575daa280537a9b29
Jonathan Sick <jsick@lsst.org>
2015-11-27 19:07:39 -0700

    Add gulp watch task
    I'm follow Chapter 9 of *Automate your Workflow* by Zell Liew. In
    particular I follow the advice of having sass run on gulp watch trigger.

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,15 @@ gulp.task('hello', function() {
     console.log('Hello Jonathan!');
 });
 
+// gulp sass - build Sass in sass/ directory and install into theme
 gulp.task('sass', function() {
     return gulp.src('sass/**/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('lsst_sphinx_technote_theme/static/css'))
+});
+
+// gulp watch - build when files change
+// sass is run automatically when watch is run; even before files are changed
+gulp.task('watch', ['sass'], function() {
+    gulp.watch('sass/**/*.scss', ['sass']);
 });
